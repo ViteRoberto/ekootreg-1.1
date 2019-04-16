47e4a7bb652242119aa1026d1c3fc00bdebf288b
ViteRoberto <roberto.vite@hotmail.com>
2019-04-16 02:27:17 -0500

    Configuración de Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBMThbndx0lTSPQEzjBw3b8M4ZeK9rxMMA",
+    authDomain: "ekootapp-1-2.firebaseapp.com",
+    databaseURL: "https://ekootapp-1-2.firebaseio.com",
+    projectId: "ekootapp-1-2",
+    storageBucket: "ekootapp-1-2.appspot.com",
+    messagingSenderId: "980791693184"    
+  }
 };
 
 /*
